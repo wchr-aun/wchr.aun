3581412fb9ac0f04ddeff57decc1cdf25c210df2
anaunz <anaunz@gmail.com>
2020-07-26 00:20:40 +0700

    ✨ Renovate the website

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeModule } from './home/home.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class HomeModule { }
